3070fd6d04f51a3433dcc1de77151787a66517eb
Hurera Ranjha <hurera@swiftcontrol.systems>
2026-04-14 13:30:41 -0400

    Finished Exercise 3 Test Statistics

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,32 @@
 package com.pluralsight;
 
-public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[] listOfNum = new int[10];
+        for (int i = 0; i < 10; i++)
+        {
+            listOfNum[i] = (int)((Math.random() * 10 ) + 1);
+        }
+        int totalNum = 0;
+        int highest = listOfNum[0];
+        int lowest = listOfNum[0];
+
+        for (int i = 0; i < 10; i++)
+        {
+            totalNum += listOfNum[i];
+            if (listOfNum[i] > highest)
+            {
+                highest = listOfNum[i];
+            }
+            if (listOfNum[i] < lowest)
+            {
+                lowest = listOfNum[i];
+            }
+        }
+
+        double average = totalNum / 10.0;
+        System.out.printf("Average: %.2f\nHigh Score: %d\nLow Score: %d\n", average, highest, lowest);
+    }
 }
