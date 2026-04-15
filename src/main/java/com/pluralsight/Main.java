522fe0c7a99db777275fe1b9ee7d5f432216eb60
Hurera Ranjha <hurera@swiftcontrol.systems>
2026-04-15 14:32:13 -0400

    Added median also hardcoded array values

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,14 +1,16 @@
 package com.pluralsight;
 
+import java.util.Arrays;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        int[] listOfNum = new int[10];
-        for (int i = 0; i < 10; i++)
-        {
-            listOfNum[i] = (int)((Math.random() * 10 ) + 1);
-        }
+        int[] listOfNum = {96, 93, 40, 100, 97, 92, 95, 98, 94, 90};
+//        for (int i = 0; i < 10; i++)
+//        {
+//            listOfNum[i] = (int)((Math.random() * 10 ) + 1);
+//        }
         int totalNum = 0;
         int highest = listOfNum[0];
         int lowest = listOfNum[0];
@@ -28,5 +30,14 @@ public class Main
 
         double average = totalNum / 10.0;
         System.out.printf("Average: %.2f\nHigh Score: %d\nLow Score: %d\n", average, highest, lowest);
+
+        double median = 0.0;
+        Arrays.sort(listOfNum);
+        int lengthOfList = listOfNum.length;
+        median += (listOfNum[lengthOfList/2 - 1] + listOfNum[lengthOfList/2])/2.0;
+
+        System.out.println("The median is: " + median);
+        System.out.println("The difference between the average and median is " + (median - average));
+
     }
 }
